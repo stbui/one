c31c9cd7c518f6699c2d7cf3350163b5098f19f6
stbui <w431106@163.com>
2020-10-12 19:00:52 +0800

    feat: wip

--- packages/one-core/src/run.ts
+++ packages/one-core/src/run.ts
@@ -4,24 +4,42 @@
  * https://github.com/stbui/apm
  */
 
-import { Container } from './container';
-import { DependenciesScanner } from './scanner';
-import { Injector } from './injector';
+import { Container } from "./container";
+import { DependenciesScanner } from "./scanner";
+import { Injector } from "./injector";
 
 /**
  * this is Runner.
  */
 export class Runner {
-    private static container = new Container();
-    private static dependenciesScanner = new DependenciesScanner(Runner.container);
-    private static injector = new Injector(Runner.container);
+  private static container = new Container();
+  private static dependenciesScanner = new DependenciesScanner(
+    Runner.container
+  );
+  private static injector = new Injector(Runner.container);
 
-    /**
-     * 开始分析模块依赖
-     * @param {object} module - this is a module.
-     */
-    static run(module) {
-        this.dependenciesScanner.scan(module);
-        this.injector.createInstancesOfDependencies();
-    }
+  /**
+   * 开始分析模块依赖
+   * @param {object} module - this is a module.
+   */
+  static run(module) {
+    this.dependenciesScanner.scan(module);
+    this.injector.createInstancesOfDependencies();
+
+    this.setupRoutes();
+  }
+
+  static setupRoutes() {
+    const modules = this.container.getModules();
+
+    modules.forEach((module: any, moduleName: any) => {
+      this.setupRouters(module.controllers, moduleName);
+    });
+  }
+
+  static setupRouters(routes, moduleName: string) {
+    routes.forEach((route) => {
+      console.log(routes);
+    });
+  }
 }
