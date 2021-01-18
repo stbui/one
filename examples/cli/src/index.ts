f903fbe5aaf0e7a3e5dcc843c187dea57ec1f9ed
王明 <wangming001@zhongan.com>
2021-01-18 22:42:29 +0800

    chore: wip

--- examples/cli/src/index.ts
+++ examples/cli/src/index.ts
@@ -1,11 +1,31 @@
+import { join } from 'path';
+
 import { Factory } from '@stbui/one-cli';
 import { DevCommand } from './dev';
 import { StartCommand } from './start';
 import { VersionCommand } from './version';
 import { InitCommand } from './init';
 
+function importExtendCommand(dependencies: object): any[] {
+    return Object.keys(dependencies)
+        .filter((name: string) => /^(@stbui\/)one-cli-/.test(name))
+        .map(name => require(name));
+}
+
+function resoveDependencies() {
+    const APP_PATH = process.cwd();
+    const PKG_PATH = join(APP_PATH, 'package.json');
+
+    const packageModule = require(PKG_PATH);
+    const dependencies = { ...packageModule.dependencies, ...packageModule.devDependencies };
+
+    return importExtendCommand(dependencies);
+}
+
 function bootstrap() {
-    Factory.create([InitCommand, DevCommand, StartCommand, VersionCommand]);
+    const extendCommand = resoveDependencies();
+
+    Factory.create([InitCommand, DevCommand, StartCommand, VersionCommand, ...extendCommand]);
 }
 
 bootstrap();
---
+++ scripts/link.sh
@@ -0,0 +1,2 @@
+ln -s /usr/local/bin/cli /usr/local/lib/node_modules/cli/bin/cli.js
+ln -s /usr/local/lib/node_modules/cli ${pwd}
